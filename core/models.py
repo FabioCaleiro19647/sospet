94955ab5b66a8d048b39d0ab8011557db8bdb23b
fabio caleiro <fabiocaleiro17@gmail.com>
2020-12-02 13:30:43 +0000

    aula:2-12-2020

--- core/models.py
+++ core/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Pet(models.Model):
-    city models.CharField(max_length=100)
+    city =  models.CharField(max_length=100)
     description = models.TextField()
     phone = models.CharField(max_length=11,)
     email = models.EmailField()
-    begin_date = models.DateTimeField(auto_now_odd=True)
-    photo = models.ImageField()
+    begin_date = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to='pet')
     active = models.BooleanField(default=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     
